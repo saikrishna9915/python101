57d648d42d8880f16c5ea43922bbb9363091a94c
saikrishna9915 <vamsi.sret@gmail.com>
2018-07-03 13:27:27 +0530

    add

---
+++ ex4.py
@@ -0,0 +1,8 @@
+#good frist program
+print("hello world")
+print("hello again")
+print("i like typing this")
+print("this is fun")
+print('Yay! printing.')
+print("I'd much rather you 'not'.")
+print('I; "said" do not touch this.')
