aeb1b335b537599974666279fb23854159c859bc
saikrishna9915 <vamsi.sret@gmail.com>
2018-07-04 12:04:10 +0530

    adding ex5 file to my girhub

---
+++ ex5.py
@@ -0,0 +1,20 @@
+print("may had a little lamb,")
+print("add everywhere that mary went,")
+print("." *10) # what 'd that do?
+
+
+end1 = "s"
+end2 = "a"
+end3 = "i"
+end4 = "k"
+end5 = "r"
+end6 = "i"
+end7 = "s"
+end8 = "h"
+end9 = "n"
+end10 = "a"
+
+# watch that "end" parameter at the end. (end=' ')
+# try removing it to see what happens
+print(end1 + end2 + end3 + end4 +end5 + end6,end=' ')
+print(end7 + end8 + end9 + end10)
